a9ccfb24928ca6b8dd0e96d8aebf8a3d9ff67240
SergeBG1974 <sergebg@yandex.ru>
2021-02-24 22:59:39 +0300

    Test1

--- vsearch.py
+++ vsearch.py
@@ -2,6 +2,6 @@ def search4letters(phrase: str, letters: str='aeiou') -> set:
     """ Возвращает множество букв из 'letters' найденных в указанной фразе
     :param phrase: Фраза для разбора
     :param letters: Наюор букв, которые требуется найти во фразе, по умолчанию - все гласные
-    :return: возвращаем в виде множества
+    :return: возвращаем в виде множества.
     """
     return set(letters).intersection(set(phrase))
